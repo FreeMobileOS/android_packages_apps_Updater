519caac8210180c23cc69cb6ddc3b8ef016717de
Satish Patel <tosatishpatel@gmail.com>
2018-03-16 10:34:47 +0530

    Changes constants to standard AOSP constants
    build prop constant changed to standard AOSP constants
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- src/org/fmo/updater/misc/Constants.java
+++ src/org/fmo/updater/misc/Constants.java
@@ -32,11 +32,10 @@ public final class Constants {
     public static final String UNCRYPT_FILE_EXT = ".uncrypt";
 
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.lineage.build.version";
+    public static final String PROP_BUILD_VERSION = "ro.build.version.release";
     public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.lineage.device";
+    public static final String PROP_DEVICE = "ro.product.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.lineage.releasetype";
-    public static final String PROP_UPDATER_URI = "lineage.updater.uri";
-
+    public static final String PROP_RELEASE_TYPE = "ro.build.type";
+    public static final String PROP_UPDATER_URI = "fmo.updater.uri";
 }
