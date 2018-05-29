6e5433df486ac888ad25d23543d742da1b7df25b
Satish Patel <tosatishpatel@gmail.com>
2018-05-29 16:54:58 +0530

    Revert "Fix build error"
    This reverts commit ae5f2507672b36cf42faf88fbd1bc8b48528ed2a.

--- Android.mk
+++ Android.mk
@@ -6,8 +6,6 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_SDK_VERSION := system_current
-
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-design \
     android-support-v4 \
